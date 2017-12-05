1059b36bf0c1e9799490bc57ad066ea38f64d3c8
Indraprahastha <indraprahastha@rocketmail.com>
2017-12-05 20:27:05 +0700

    Detailtransaksi

--- components/Detailtransaksi.js
+++ components/Detailtransaksi.js
@@ -36,34 +36,61 @@ export default class Detailtransaksi extends React.Component {
 
   render() {
     const dataMentah = this.props.navigation.state.params.list.items
-    const list = dataMentah
+    const list = []
 
-    // for (var i = 0; i < dataMentah.length; i++) {
-    //   if (true) {
-    //
-    //   } else if (true) {
-    //
-    //   } else if (true) {
-    //
-    //   } else if (true) {
-    //
-    //   } else if (true) {
-    //
-    //   } else if (true) {
-    //
-    //   } else if (true) {
-    //
-    //   } else if (true) {
-    //
-    //   } else if (true) {
-    //
-    //   } else if (true) {
-    //
-    //   } else if (true) {
-    //
+    for (var i = 0; i < dataMentah.length; i++) {
+      if (dataMentah[i].category == "Food/Meal Solutions, Grains & Pasta/Grains & Rice") {
+        dataMentah[i].avatar = 'http://ciat.cgiar.org/wp-content/uploads/2016/07/RICE-icon.png'
+        list.push(dataMentah[i])
+      } else if (dataMentah[i].category == 'Food/Meal Solutions, Grains & Pasta/Pasta & Noodles') {
+        dataMentah[i].avatar = 'https://d30y9cdsu7xlg0.cloudfront.net/png/82812-200.png'
+        list.push(dataMentah[i])
+      } else if (dataMentah[i].category == 'Food/Beverages/Powdered Drinks & Mixes') {
+        dataMentah[i].avatar = 'http://icons.veryicon.com/png/System/Icons8%20Metro%20Style/Hot%20Beverages%20Tea.png'
+        list.push(dataMentah[i])
+      } else if (dataMentah[i].category == 'Home/Bath/Shower Curtains') {
+        dataMentah[i].avatar = 'https://d30y9cdsu7xlg0.cloudfront.net/png/51-200.png'
+        list.push(dataMentah[i])
+      } else if (dataMentah[i].category == 'Personal Care/Bath & Body/Body Wash & Cleansers') {
+        dataMentah[i].avatar = 'https://d30y9cdsu7xlg0.cloudfront.net/png/26970-200.png'
+        list.push(dataMentah[i])
+      } else if (dataMentah[i].category == 'Food/Beverages/Soft Drinks') {
+        dataMentah[i].avatar = 'https://marketplace.canva.com/MACaIgKSFxQ/1/thumbnail_large/canva-burger-with-soft-drink-icon-MACaIgKSFxQ.png'
+        list.push(dataMentah[i])
+      } else if (dataMentah[i].category == 'Pets/Dogs/Dog Food') {
+        dataMentah[i].avatar = 'http://downloadicons.net/sites/default/files/dog-icon-86451.png'
+        list.push(dataMentah[i])
+      } else if (dataMentah[i].category == 'Baby/Diapering/Diapers/Disposable Diapers') {
+        dataMentah[i].avatar = 'https://d30y9cdsu7xlg0.cloudfront.net/png/75158-200.png'
+        list.push(dataMentah[i])
+      } else if (dataMentah[i].category == 'Food/Fresh Food/Dairy, Eggs & Cheese/Milk & Cream') {
+        dataMentah[i].avatar = 'http://free-icon-rainbow.com/i/icon_05205/icon_052050_256.png'
+        list.push(dataMentah[i])
+      } else if (dataMentah[i].category == 'Home Improvement/Hardware/Door Hardware/Door Hinges') {
+        dataMentah[i].avatar = 'https://www.shareicon.net/data/512x512/2015/10/02/110903_processor_512x512.png'
+        list.push(dataMentah[i])
+      } else if (dataMentah[i].category == 'Food/Baking/Oil & Shortening') {
+        dataMentah[i].avatar = 'https://dtgxwmigmg3gc.cloudfront.net/files/51bacb46c566d724c1018885-icon-256x256.png'
+        list.push(dataMentah[i])
+      } else if (dataMentah[i].category == 'Auto & Tires/Oils and Fluids/Motor Oil') {
+        dataMentah[i].avatar = 'https://www.texasrefinery.com/wp-content/uploads/2015/09/engine-oil-icon-2.png.png'
+        list.push(dataMentah[i])
+      } else if (dataMentah[i].category == 'Food/Breakfast & Cereal/Oatmeal & Hot Cereal') {
+        dataMentah[i].avatar = 'https://cdn3.iconfinder.com/data/icons/breakfast-icons/580/Cereal_Bowl_And_Spoon-512.png'
+        list.push(dataMentah[i])
+      }
+    }
+
+    // function objekBuild (data) {
+    //   let object = {
+    //     name: data.name,
+    //     price: data.price,
+    //     qty: data.qty,
+    //     avatar: data
     //   }
+    //   return object
     // }
-    
+
     return (
           <View style={{paddingTop: 20, paddingLeft: 15, paddingRight: 15, flexDirection: 'row'}}>
           <Container>
@@ -75,7 +102,7 @@ export default class Detailtransaksi extends React.Component {
                 <ListItem
                 roundAvatar
                 hideChevron={true}
-                avatar={{uri:'https://i1.sndcdn.com/artworks-000066312689-d7quy2-t500x500.jpg'}}
+                avatar={{uri:item.avatar}}
                 key={index}
                 title={item.name}
                 subtitle={"Rp." + item.price + " ("+item.qty+")"}
